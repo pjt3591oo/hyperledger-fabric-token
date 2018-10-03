093bf3b8673b2ee6d6dd374e971170b958d1c28d
pjt3591oo <pjt3591oo@gmail.com>
2018-10-04 08:13:20 +0900

    invoke 파일을 수정한다
    이벤트 설정하는 부분을 삭제한다

--- API/utils/invoke.js
+++ API/utils/invoke.js
@@ -31,95 +31,90 @@ var tx_id = null;
 // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 
 function invoke ({
-    fcn,
-    args
+	fcn,
+	args
 }) {
-    let data;
-    return new Promise((resolve, reject) => {
-        Fabric_Client.newDefaultKeyValueStore({ path: store_path
-        }).then((state_store) => {
-            // assign the store to the fabric client
-            fabric_client.setStateStore(state_store);
-            var crypto_suite = Fabric_Client.newCryptoSuite();
-            // use the same location for the state store (where the users' certificate are kept)
-            // and the crypto store (where the users' keys are kept)
-            var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
-            crypto_suite.setCryptoKeyStore(crypto_store);
-            fabric_client.setCryptoSuite(crypto_suite);
 
-            // get the enrolled user from persistence, this user will sign all requests
-            return fabric_client.getUserContext('user2', true);
-        }).then((user_from_store) => {
-            if (user_from_store && user_from_store.isEnrolled()) {
-                console.log('Successfully loaded user from persistence');
-                member_user = user_from_store;
-            } else {
-                throw new Error('Failed to get user.... run registerUser.js');
-            }
+	let data;	
 
-            // get a transaction id object based on the current user assigned to fabric client
-            tx_id = fabric_client.newTransactionID();
-            console.log("Assigning transaction_id: ", tx_id._transaction_id);
+	return new Promise((resolve, reject) => {
+		Fabric_Client.newDefaultKeyValueStore({ path: store_path
+		}).then((state_store) => {
+			// assign the store to the fabric client
+			fabric_client.setStateStore(state_store);
+			var crypto_suite = Fabric_Client.newCryptoSuite();
+			// use the same location for the state store (where the users' certificate are kept)
+			// and the crypto store (where the users' keys are kept)
+			var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+			crypto_suite.setCryptoKeyStore(crypto_store);
+			fabric_client.setCryptoSuite(crypto_suite);
+		
+			// get the enrolled user from persistence, this user will sign all requests
+			return fabric_client.getUserContext('user2', true);
+		}).then((user_from_store) => {
+			if (user_from_store && user_from_store.isEnrolled()) {
+				console.log('Successfully loaded user from persistence');
+				member_user = user_from_store;
+			} else {
+				throw new Error('Failed to get user.... run registerUser.js');
+			}
+		
+			// get a transaction id object based on the current user assigned to fabric client
+			tx_id = fabric_client.newTransactionID();
+			console.log("Assigning transaction_id: ", tx_id._transaction_id);
+		
+			var request = {
+				chaincodeId: 'token', // -n 이름옵션
+				fcn: fcn,
+				args: args,
+				chainId: 'ydp',  // -C 채널옵션
+				txId: tx_id
+			};
+			// send the transaction proposal to the peers
+			return channel.sendTransactionProposal(request);
+		}).then((results) => {
+			
+			var proposalResponses = results[0];
+			var proposal = results[1];
+			let isProposalGood = false;
 
-            var request = {
-                chaincodeId: 'token', // -n 이름옵션
-                fcn: fcn,
-                args: args,
-                chainId: 'ydp',  // -C 채널옵션
-                txId: tx_id
-            };
-            // send the transaction proposal to the peers
-            return channel.sendTransactionProposal(request);
-        }).then((results) => {
-
-            var proposalResponses = results[0];
-            var proposal = results[1];
-            let isProposalGood = false;
-
-            if (proposalResponses && proposalResponses[0].response &&
-                proposalResponses[0].response.status === 200) {
-                    isProposalGood = true;
-                    console.log('Transaction proposal was good');
-                    data = proposalResponses[0].response.payload.toString()
+			if (proposalResponses && proposalResponses[0].response &&
+				proposalResponses[0].response.status === 200) {
+					isProposalGood = true;
+					console.log('Transaction proposal was good');
+					data = proposalResponses[0].response.payload.toString()
 			} else {
 				console.error('Transaction proposal was bad');
 				data = proposalResponses[0].details
 			}
 
-            if (isProposalGood) {
-                console.log(util.format(
-                    'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
-                    proposalResponses[0].response.status, proposalResponses[0].response.message));
+			if (isProposalGood) {
+				console.log(util.format(
+					'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
+					proposalResponses[0].response.status, proposalResponses[0].response.message));
+		
+				// build up the request for the orderer to have the transaction committed
+				var request = {
+					proposalResponses: proposalResponses,
+					proposal: proposal
+				};
 
-                // build up the request for the orderer to have the transaction committed
-                var request = {
-                    proposalResponses: proposalResponses,
-                    proposal: proposal
-                };
+				var sendPromise = channel.sendTransaction(request);
 
-                var transaction_id_string = tx_id.getTransactionID(); //Get the transaction ID string to be used by the event processing
-                var promises = [];
-
-                var sendPromise = channel.sendTransaction(request);
-                promises.push(sendPromise); //we want the send transaction first, so that we know where to check status
-
-                let event_hub = fabric_client.newEventHub();
-                event_hub.setPeerAddr('grpc://127.0.0.1:7053');
-
-                return Promise.all(promises);
-            } else {
-                console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-                throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-            }
-        }).then((results) => {
-            console.log('Send transaction commited completed');
-            // check the results in the order the promises were added to the promise all list
-            resolve(data)
-        }).catch((err) => {
-            console.log('Send transaction commited failed')
-            reject(err)
-        })
-    })
+				return Promise.all( [sendPromise] );
+			} else {
+				console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+				throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+			}
+		}).then((results) => {
+			console.log('Send transaction commited completed');
+			// check the results in the order the promises were added to the promise all list
+			resolve(data)	
+		}).catch((err) => {
+			console.log(`Send transaction commited failed: ${err}`)
+			reject(data)
+		})
+	})	
 }
 
 module.exports = invoke
